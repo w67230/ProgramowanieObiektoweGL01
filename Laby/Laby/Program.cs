996fa1e00aa67502cf98307295a87b7182eef4f1
w67230 <w67230@student.wsiz.edu.pl>
2023-10-04 12:58:25 +0200

    lab

---
+++ Laby/Laby/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
